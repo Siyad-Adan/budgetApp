9280da3083553870fd609f5e1bc15c71e17d07c6
Siyad Adan <siyad.adan94@gmail.com>
2020-08-05 17:01:46 -0400

    ui and data layer interactions in the global controller

--- app.js
+++ app.js
@@ -26,17 +26,21 @@ var budgetController = (function () {
   return {
     addItem: function (type, des, val) {
       var newItem;
-      ID = 0;
+      if (data.allItems[type].length !== 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 1;
+      }
+
       if (type === "exp") {
         newItem = new Expense(ID, des, val);
       } else {
-        newItem = new Income(ID, des, value);
+        newItem = new Income(ID, des, val);
       }
 
       data.allItems[type].push(newItem);
-    },
-    getExpenses: function () {
-      return data.allItems.exp;
+
+      return newItem;
     },
   };
 })();
@@ -46,8 +50,10 @@ var UIController = (function () {
   var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
-    inputValue: ".add__type",
+    inputValue: ".add__value",
     inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list",
   };
 
   return {
@@ -58,8 +64,42 @@ var UIController = (function () {
         value: document.querySelector(DOMstrings.inputValue).value,
       };
     },
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+
+        html =
+          '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+
+        html =
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      //Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
     getDOMStrings: function () {
       return DOMstrings;
+    },
+    clearFields: function () {
+      var fields, fieldsArr;
+      // this is a list
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+
+      // how to convert from list to array;
+      fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach(function (current, index, array) {
+        current.value = "";
+      });
     },
   };
 })();
@@ -72,17 +112,20 @@ var controller = (function (budgetCtrl, UICtrl) {
     document.addEventListener("keypress", function (event) {
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
-        console.log(DOM);
       }
     });
   };
 
   var ctrlAddItem = function () {
+    var input, newItem;
     // Get filed input data
-    var input = UICtrl.getinput();
-    console.log(input);
+    input = UICtrl.getinput();
     //add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //add the new item to the ui
+    newListItem = UICtrl.addListItem(newItem, input.type);
+    //clear the fields
+    UICtrl.clearFields();
     //calculate the budget
     //display the budget
   };
